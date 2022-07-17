9ebdf1b179e52fa99b07adc28947e7a1977ed900
Budianto <budiantojang@gmail.com>
2022-07-17 14:04:13 +0700

    added validation for Customer Model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
-  has_many :orders
-end
+    has_many :orders
+  
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  end
+  
