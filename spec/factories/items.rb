9be323d90cb65019cac30d06e489884da26e15c1
Budianto <budiantojang@gmail.com>
2022-07-17 13:49:06 +0700

    generated test model for item using faker

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    price { Faker::Number.decimal }
   end
 end
