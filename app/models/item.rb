410d63ec03e8ae9617352bff6777d6e4eb4f56f1
Budianto <budiantojang@gmail.com>
2022-07-17 13:49:45 +0700

    added validation for item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-    has_many :order_details
-    has_many :orders, through: :order_details
-    has_many :item_categories
-    has_many :categories, through: :item_categories
-  end
-  
+  has_many :order_details
+  has_many :orders, through: :order_details
+  has_many :item_categories
+  has_many :categories, through: :item_categories
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :description, length: { maximum: 150 }
+end
