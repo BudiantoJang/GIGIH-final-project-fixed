ccd38c612c7c6fd80dc895a12a62bc1e006cfd5c
Budianto <budiantojang@gmail.com>
2022-07-17 15:45:26 +0700

    updated byTotal function in Daily controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
-    belongs_to :customer
-    has_many :order_details
-    has_many :items, through: :order_details
-  
-    def self.change_status_be_canceled
-      where.not(status: "PAID").and(Order.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)).update_all(status: "CANCELED")
-    end
+  belongs_to :customer
+  has_many :order_details
+  has_many :items, through: :order_details
+
+  def self.get_order_now
+    where(created_at: Date.today.beginning_of_day..Date.today.end_of_day).order(created_at: :asc)
   end
-  
+
+  def self.change_status_be_canceled
+    where.not(status: "PAID").and(Order.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)).update_all(status: "CANCELED")
+  end
+end
+    
